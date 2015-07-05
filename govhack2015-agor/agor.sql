5369d34fb96a083dd7754331cf4d34938a52f102
Lyndon D'Arcy <lyndon.darcy@gmail.com>
2015-07-05 15:22:13 +1000

    SQL to correct MediaWiki timestamps

---
+++ govhack2015-agor/agor.sql
@@ -0,0 +1,23 @@
+﻿update mediawiki.revision 
+set rev_timestamp = timestamp '2014-12-11 00:00:00-00'
+where rev_id in (
+select old_id from mediawiki.pagecontent where old_text like '%revision_date=2014-12-11%'
+);
+
+update mediawiki.revision 
+set rev_timestamp = timestamp '2015-02-16 00:00:00-00'
+where rev_id in (
+select old_id from mediawiki.pagecontent where old_text like '%revision_date=2015-02-16%'
+);
+
+update mediawiki.revision 
+set rev_timestamp = timestamp '2015-04-24 00:00:00-00'
+where rev_id in (
+select old_id from mediawiki.pagecontent where old_text like '%revision_date=2015-04-24%'
+);
+
+update mediawiki.revision 
+set rev_timestamp = timestamp '2015-05-21 00:00:00-00'
+where rev_id in (
+select old_id from mediawiki.pagecontent where old_text like '%revision_date=2015-05-21%'
+);
